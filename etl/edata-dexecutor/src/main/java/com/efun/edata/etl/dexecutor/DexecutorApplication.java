0560f0c071da6f560376cdec7d53cabc2a1fc83e
kinven <xqw2@163.com>
2018-04-17 08:19:08 +0800

    rest hello

---
+++ etl/edata-dexecutor/src/main/java/com/efun/edata/etl/dexecutor/DexecutorApplication.java
@@ -0,0 +1,16 @@
+package com.efun.edata.etl.dexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kinven on 2018/4/17.
+ */
+@SpringBootApplication
+public class DexecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DexecutorApplication.class,args);
+    }
+
+}
