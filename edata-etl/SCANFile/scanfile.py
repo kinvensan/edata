ae307e83a3a8b6de267f2466feaeb507078f6248
kinvensan <xqw2@163.com>
2018-04-24 16:00:19 +0800

    nsqlookup

---
+++ edata-etl/SCANFile/scanfile.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import nsq
+
+def handler(message):
+    print message
+    return True
+
+r = nsq.Reader(message_handler=handler,
+        lookupd_http_addresses=['http://127.0.0.1:4161'],
+        topic='test', channel='asdfxx', lookupd_poll_interval=15)
+nsq.run()
---
+++ edata-etl/SCANFile/sync.py
@@ -0,0 +1,9 @@
+
+def synccommand():
+    print 'hello'
+
+def unzipfile():
+    print "world"
+
+def mvfile():
+    print "ok"
